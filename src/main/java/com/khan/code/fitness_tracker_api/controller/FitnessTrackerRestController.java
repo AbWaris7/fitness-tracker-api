d6e3d1d0b4ac0576a354c69777403e02964ba992
abwaris7 <hellokhan.ch@gmail.com>
2025-06-19 06:29:31 +0200

    add validation to FitnessTrackerRestController

--- src/main/java/com/khan/code/fitness_tracker_api/controller/FitnessTrackerRestController.java
+++ src/main/java/com/khan/code/fitness_tracker_api/controller/FitnessTrackerRestController.java
@@ -2,30 +2,40 @@ package com.khan.code.fitness_tracker_api.controller;
 
 import com.khan.code.fitness_tracker_api.dao.FitnessTrackerDAO;
 import com.khan.code.fitness_tracker_api.entity.FitnessTracker;
+import com.khan.code.fitness_tracker_api.service.FitnessTrackerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/tracker")
+@Tag(name="Fitness Tracker Rest API Endpoint", description = "Operations related to Fitness Tracker")
 public class FitnessTrackerRestController {
 
-    private final FitnessTrackerDAO fitnessTrackerDAO;
+    private final FitnessTrackerService fitnessTrackerService;
 
-    @Autowired
-    public FitnessTrackerRestController(FitnessTrackerDAO fitnessTrackerDAO) {
-        this.fitnessTrackerDAO = fitnessTrackerDAO;
+    public FitnessTrackerRestController(FitnessTrackerService fitnessTrackerService) {
+        this.fitnessTrackerService = fitnessTrackerService;
     }
 
+    @Operation(summary = "Get all Fitness Tracker", description = "Retrieve a list of all users details ")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping()
     public List<FitnessTracker> findAll() {
-        return fitnessTrackerDAO.findAll();
+        return fitnessTrackerService.findAll();
     }
 
+    @Operation(summary = "Create a new Fitness Tracker", description = "Add a new user details")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/add")
-    public FitnessTracker save(@RequestBody FitnessTracker fitnessTracker) {
-        return fitnessTrackerDAO.save(fitnessTracker);
+    public FitnessTracker save(@Valid @RequestBody FitnessTracker fitnessTracker) {
+        return fitnessTrackerService.save(fitnessTracker);
     }
 
 
