52a5b41089a70c1ce324c0a6683428b8f5122267
abwaris7 <hellokhan.ch@gmail.com>
2025-06-19 06:29:05 +0200

    add validation to FitnessTrackerDTO fields

--- src/main/java/com/khan/code/fitness_tracker_api/entity/FitnessTracker.java
+++ src/main/java/com/khan/code/fitness_tracker_api/entity/FitnessTracker.java
@@ -1,6 +1,9 @@
 package com.khan.code.fitness_tracker_api.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -13,9 +16,18 @@ public class FitnessTracker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Activity is required")
     private String activity;
+
+    @Min(value = 1, message = "Duration must be greater than 0")
     private int duration;
+
+    @Min(value = 1, message = "Duration must be greater than 0")
     private int calories;
 
 }
