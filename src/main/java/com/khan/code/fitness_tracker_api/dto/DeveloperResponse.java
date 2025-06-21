0faa46f670d62a7464d5aee6db592f9624b46a60
abwaris7 <hellokhan.ch@gmail.com>
2025-06-21 16:19:39 +0200

    add DeveloperResponse DTO Class

---
+++ src/main/java/com/khan/code/fitness_tracker_api/dto/DeveloperResponse.java
@@ -0,0 +1,13 @@
+package com.khan.code.fitness_tracker_api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class DeveloperResponse {
+
+    private Long id;
+    private String email;
+}
