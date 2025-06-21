6b4bf4ef2570895740019fd527f490dec4e7d427
abwaris7 <hellokhan.ch@gmail.com>
2025-06-21 16:36:14 +0200

    code refactor

--- src/main/java/com/khan/code/fitness_tracker_api/controller/FitnessTrackerRestController.java
+++ src/main/java/com/khan/code/fitness_tracker_api/controller/FitnessTrackerRestController.java
@@ -1,13 +1,9 @@
 package com.khan.code.fitness_tracker_api.controller;
-
-import com.khan.code.fitness_tracker_api.dao.FitnessTrackerDAO;
 import com.khan.code.fitness_tracker_api.entity.FitnessTracker;
 import com.khan.code.fitness_tracker_api.service.FitnessTrackerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
