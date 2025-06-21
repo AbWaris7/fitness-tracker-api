ff2f44409c1663883a3922de23e9bbd3a34a1a61
abwaris7 <hellokhan.ch@gmail.com>
2025-06-21 16:36:40 +0200

    added SecurityConfig class for authorization purposes

---
+++ src/main/java/com/khan/code/fitness_tracker_api/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.khan.code.fitness_tracker_api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // For Postman/curl
+                .headers(headers -> headers.disable()) // To allow H2 console
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/developers/signup").permitAll()
+                        .requestMatchers("/actuator/shutdown").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/api/developers/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults()) // Enables Basic Auth
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
