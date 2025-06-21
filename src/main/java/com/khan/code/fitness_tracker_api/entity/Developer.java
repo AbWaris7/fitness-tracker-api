d9ec4bc22831f831620b0dac3a79572dba84d792
abwaris7 <hellokhan.ch@gmail.com>
2025-06-21 16:18:18 +0200

    add Developer Entity

---
+++ src/main/java/com/khan/code/fitness_tracker_api/entity/Developer.java
@@ -0,0 +1,26 @@
+package com.khan.code.fitness_tracker_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "developers", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
